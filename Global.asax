00895428d30f0521e615f16522710c921573b0f2
SavanPatel2307 <50596601+SavanPatel2307@users.noreply.github.com>
2019-05-29 22:51:26 -0400

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_200395832_Class3.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_200395832_Class3.Startup))]
+namespace _200395832_Class3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
